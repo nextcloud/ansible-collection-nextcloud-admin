c4c91afa53391d48ba192964b2bd5d8efa4dcb85
Marc Crébassa <aalaesar@gmail.com>
2025-11-11 15:10:25 +0100

    fix remaining sanity errors
    Signed-off-by: Marc Crébassa <aalaesar@gmail.com>

--- tests/unit/modules/test_user.py
+++ tests/unit/modules/test_user.py
@@ -2,9 +2,6 @@ from unittest import TestCase
 from unittest.mock import patch, MagicMock
 from ansible_collections.nextcloud.admin.plugins.modules import user
 from ansible.module_utils import basic
-from ansible_collections.nextcloud.admin.plugins.module_utils.exceptions import (
-    IdentityNotPresent,
-)
 from ansible_collections.nextcloud.admin.plugins.module_utils.identities import (
     idState,
 )
