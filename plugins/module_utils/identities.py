f6f30c3b64b28b5be58e90c21571665a6e1c9504
Marc Crébassa <aalaesar@gmail.com>
2026-09-20 16:17:57 +0200

    add forgotten annotation import
    Signed-off-by: Marc Crébassa <aalaesar@gmail.com>

--- plugins/module_utils/identities.py
+++ plugins/module_utils/identities.py
@@ -23,7 +23,7 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-# from __future__ import annotations
+from __future__ import annotations
 import json
 
 from typing import TYPE_CHECKING
